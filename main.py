d13f4495d49c405e9970439c9ab00114d58ed6bc
Andreas <Andreas.ahrlund@gmail.com>
2020-01-18 16:57:36 +0100

    Starting to extract cols

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 
 import csv
-
 import numpy
 import numpy as np
 import pandas as pd
-
+import scipy
+from scipy.stats import stats
 
 
 path = 'ResamplesT.txt'
+data = pd.read_csv(path, sep="\t")
+data_top = data.head()
+print(data_top)
 
-X = pd.read_csv(path, sep="\t")
-print(X)
+hive = data['HIVE-COTE']
+BOSS = data['BOSS']
+
+print(hive)
+
