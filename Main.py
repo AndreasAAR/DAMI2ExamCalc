54a787c8d9a85e7f11c08d2a3faa03f5ad487ab8
Andreas <Andreas.ahrlund@gmail.com>
2020-01-18 18:59:42 +0100

    Managed to sync files into git- hopefully

---
+++ Main.py
@@ -0,0 +1,23 @@
+'''
+Auth: Andreas Ährlund-Richter
+Course: Data Mining 2 "Research Topics In Data Science"
+T-test question in Hand-In Exam.
+'''
+
+import csv
+import numpy as np
+import pandas as pd
+import scipy
+from scipy.stats import stats
+
+path = 'ResamplesT.txt'
+data = pd.read_csv(path, sep="\t")
+data_top = data.head()
+print(data_top)
+
+hive = data['HIVE-COTE']
+BOSS = data['BOSS']
+
+results = stats.ttest_rel(hive,BOSS)
+print(results)
+
