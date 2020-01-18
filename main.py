486230125509d09e4eee0a710e85413167802448
Andreas <Andreas.ahrlund@gmail.com>
2020-01-18 18:31:09 +0100

    Performed paired T-test on HIVE and BOSS classifiers

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+'''
+Auth: Andreas Ährlund-Richter
+Course: Data Mining 2 "Research Topics In Data Science"
+T-test question in Hand-In Exam.
+'''
 
 import csv
-import numpy
 import numpy as np
 import pandas as pd
 import scipy
 from scipy.stats import stats
-
 
 path = 'ResamplesT.txt'
 data = pd.read_csv(path, sep="\t")
@@ -15,5 +18,5 @@ print(data_top)
 hive = data['HIVE-COTE']
 BOSS = data['BOSS']
 
-print(hive)
-
+results = stats.ttest_rel(hive,BOSS)
+print(results)
