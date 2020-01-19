0835af3fd5bf728aec7ecacf80196fffb36b2a5c
Andreas <Andreas.ahrlund@gmail.com>
2020-01-19 13:34:32 +0100

    fixed calc, corresponds to built in test

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import scipy
 from scipy.stats import stats
+import math
 
 #equations from
 #https://www.statisticshowto.datasciencecentral.com/probability-and-statistics/t-test/
@@ -28,19 +29,42 @@ print(results)
 ###"Manual" step-wise procedure
 
 #Getting squared and summed differences
-sumSquared = sum((hive**2) -(boss**2))
-sumDiff = sum(hive-boss)
-sumDiffSquared = sumSquared**2
+#sumsquaresΣD
+Σ2 = sum(((hive) - (boss)) ** 2)
+Σ = sum(hive - boss)
+Σ2_2 = Σ ** 2
 #Number of samples
-numSamp = len(hive)
+n = len(hive)
 
 print("sumDiff")
-print(sumDiff)
+print(Σ)
 print("sumSquared")
-print(sumSquared)
+print(Σ2)
 print("sumDiffSquared")
-print(sumDiffSquared)
+print(Σ2_2)
 print("numSamp")
-print(numSamp)
+print(n)
+
+#Manual calculation from results
+result = ((Σ) / n)\
+         / math.sqrt((Σ2 - (Σ2_2 / n))
+                     / ((n - 1) * (n)))
+print(result)
+
+
+'''
+ΣD:  Sum of the differences (Sum of X-Y from Step 2)
+Σ2DSum of the squared differences (from Step 4)
+(ΣD)2:  Sum of the differences (from Step 2), squared.
+N: Number of samples in each population.
+From Python https://github.com/AndreasAAR/DAMI2ExamCalc these will be:
+ΣD:  9.697089319
+Σ2D:  14.334104893544094
+(ΣD)2:  205.46656309912473
+N: 85
+Formula used:
+https://www.statisticshowto.datasciencecentral.com/probability-and-statistics/t-test/
+'''
+
 
 
